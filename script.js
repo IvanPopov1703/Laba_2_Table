53a35c06904b6afa201aa4484137d54882fc831a
Ivan Popov <popow85.papaw@gmail.com>
2020-06-10 21:54:30 +0300

    Сделал функцию для добавления строки в таблицу

--- script.js
+++ script.js
@@ -5,3 +5,49 @@ let selectedColumn = -1;
 let motoTable = d.getElementById('motorcycleTable');
 let formAdd = d.getElementById('addMotorcycle');
 
+//Функция для добавления новой строки в таблицу
+function addRow() {
+    //Считываем значение с формы
+    let inputModel = d.getElementById('inputModel');
+    let inputPower = d.getElementById('inputPower');
+    let inputVolume = d.getElementById('inputVolume');
+    let inputCost = d.getElementById('inputCost');
+
+    //Находим таблицу, в которую будем добовлять
+    let tableAdd = motoTable.getElementsByTagName('tbody')[0];
+
+    //Создаем строку таблицы и добовляем её в конец
+    let row = d.createElement("TR");
+    tableAdd.appendChild(row);
+
+    //Создаём ячейки в вышесозданной строке и добовляем их
+    let td1 = d.createElement("TD");
+    let td2 = d.createElement("TD");
+    let td3 = d.createElement("TD");
+    let td4 = d.createElement("TD");
+    let td5 = d.createElement("TD");
+
+    //Добавляем их в строку
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
+
+    let delButton = d.createElement('button'); //Создааём кнопку удалить
+    let textBtn = d.createTextNode('Удалить'); //Создаём текст для кнопки
+    delButton.appendChild(textBtn); //Записываем текст на кнопку
+    delButton.style.cssText = "margin-left: 10px; margin-top: 13px;" //Добавляем стили дял кнопки
+    row.appendChild(delButton);
+
+    //Заполняем ячейки инфой
+    td1.innerHTML = inputModel.value;
+    td2.innerHTML = inputPower.value;
+    td3.innerHTML = inputVolume.value;
+    td4.innerHTML = inputCost.value;
+}
+
+//Клик на кнопку добавить
+formAdd.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addRow();
+});
